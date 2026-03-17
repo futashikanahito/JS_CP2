e8e5883f6009fe3e029b71583bcf17a3bbb823be
JS-CP1 <joseph.stratford@ucas-edu.net>
2026-03-17 09:15:14 -0600

    testing new computer commit

--- notes/classes.py
+++ notes/classes.py
@@ -8,12 +8,27 @@ class Animal:
         self.age = age
     
     def __str__(self):
-        return f"Name = {self.name}, \nSpecies = {self.species}, \nAge = {self.age}"
+        return f"Name = {self.name}, \nSpecies = {self.species}, \nAge = {self.age}\n"
 
     def birthday(self):
         self.age += 1
 
 dog = Animal("Doug", "Dog", 4)
 bunny = Animal("Judy", "Rabbit", 20)
-print(dog)
-print(bunny)
+#print(dog)
+#print(bunny)
+#dog.birthday()
+#print(dog)
+
+# Ex 2
+class ClassPeriod:
+    def __init__(self, subject = None, teacher = None, room = None):
+        self.subject = subject.capitalize()
+        self.teacher = teacher
+        self.room = room
+    
+    def __str__(self):
+        return f"Subject: {self.subject}, \nTeacher: {self.teacher}, \nRoom: {self.room}"
+
+first = ClassPeriod(subject = "Computer Programming 2", teacher = "Ms. LaRose", room = "200")
+print(first)
