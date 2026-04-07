6b44331b176477f0c4e466355ba60f0491531f95
JS-CP1 <joseph.stratford@ucas-edu.net>
2026-04-07 08:58:58 -0600

    37 GUI Notes

---
+++ notes/gui.py
@@ -0,0 +1,30 @@
+# JS, 1st, GUI with Tkinter
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Testing/Notes")
+root.configure(background="lightpink")
+root.minsize(500, 500)
+root.maxsize(1000, 1000)
+root.geometry("300x300+1125+750")
+
+label = tk.Label(root, text="This is currently working!", font=("Times New Roman", 14, "italic"))
+label.config(background="lightpink", fg="black")
+
+image = tk.PhotoImage(file="csvs, txts, sprites/bread.gif")
+tk.Label(root, image=image).pack()
+
+# ----------------- Button --------------------
+root.count = 0
+
+def add():
+    root.count += 1
+    num = tk.Label(root, text=root.count)
+    num.config(background="lightpink", fg="black")
+    num.pack()
+
+btn = tk.Button(root, text="ADD", command=add)
+btn.pack()
+
+label.pack()
+root.mainloop()
